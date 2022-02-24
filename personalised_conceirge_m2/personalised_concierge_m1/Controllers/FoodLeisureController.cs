d4d12e583e0c9c3165bed8781c6f548bbe00dcdb
mayinot <31657679+mayinot@users.noreply.github.com>
2022-02-24 17:14:04 +0800

    added the html layout for specific item page

---
+++ personalised_conceirge_m2/personalised_concierge_m1/Controllers/FoodLeisureController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace personalised_concierge_m1.Controllers
+{
+    public class FoodLeisureController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SpecificFoodLeisure(string FoodLeisureID)
+        {
+            if(FoodLeisureID != null)
+            {
+                return View();
+            }
+            return Content(FoodLeisureID);
+        }
+    }
+}
