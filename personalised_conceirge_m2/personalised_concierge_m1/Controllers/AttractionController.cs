f246c303ecf8f96f4eb821e3fe9eda35c93b4e10
Kenrick78 <kenong7878@gmail.com>
2022-03-23 13:24:31 +0800

    Created Attraction Controller

---
+++ personalised_conceirge_m2/personalised_concierge_m1/Controllers/AttractionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using personalised_concierge_m1.Models.Entities.Attraction;
+
+namespace personalised_concierge_m1.Controllers
+{
+    public class AtttractionController : Controller
+    {
+        private Atttraction attraction = new Attraction();
+
+        public IActionResult Index()
+        {
+            return View(attraction);
+        }
+    }
+}
