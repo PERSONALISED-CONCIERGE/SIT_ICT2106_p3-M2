9239d73f9a2fb1708b1564c988f9b41561a4c2f4
DanialAshidiq <77533235+DanialAshidiq@users.noreply.github.com>
2022-03-28 18:15:38 +0800

    Updating ItineraryItem

---
+++ personalised_conceirge_m2/personalised_concierge_m1/Models/Entities/Navigation/Navigation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.VisualBasic;
+using personalised_concierge_m1.Models.Entities.UserDetails;
+namespace personalised_concierge_m1.Models.Entities.Navigation
+{
+    public class Navigation
+    {
+
+        [Key]
+        public int navigation_id { get; set; }
+
+        [Required]
+        public decimal start_lon { get; set; }
+
+        [Required]
+        public decimal start_lat { get; set; }
+
+        [Required]
+        public decimal end_lon { get; set; }
+
+        [Required]
+        public decimal end_lat { get; set; }
+
+        [Required]
+        public decimal duration { get; set; }
+
+        [Required]
+        public decimal distance { get; set; }
+
+        [Required]
+        public int type { get; set; }
+
+
+    }
+}
