1e90d21954033ba550949bbb83f7c0ae70d5f3d6
kendricklq <2001389@sit.singaporetech.edu.sg>
2022-03-31 12:16:08 +0800

    Getting default mode of transport based on distance

---
+++ personalised_conceirge_m2/personalised_concierge_m1/Models/Entities/Navigation/INavigation.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace personalised_concierge_m1.Models.Entities.Navigation
+{
+    public interface INavigation
+    {
+        public Navigation getDistance(string endPoint);
+    }
+}
