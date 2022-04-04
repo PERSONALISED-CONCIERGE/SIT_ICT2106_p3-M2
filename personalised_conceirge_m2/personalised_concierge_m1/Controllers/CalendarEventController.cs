6477253a90943b1c519d186da4893211fb159961
chenghao99 <chenghao99@users.noreply.github.com>
2022-04-04 16:09:54 +0800

    remove unused func

--- personalised_conceirge_m2/personalised_concierge_m1/Controllers/CalendarEventController.cs
+++ personalised_conceirge_m2/personalised_concierge_m1/Controllers/CalendarEventController.cs
@@ -68,49 +68,6 @@ namespace personalised_concierge_m1.Controllers
 
         }
 
-        // convert string of e.g "05/01/2009 06:32:00" to DateTime
-        private DateTime convertStringToDateTime(string dateTime)
-        {
-            DateTime dateValue;
-            DateTime.TryParse(dateTime, out dateValue);
-            return dateValue;
-        }
-
-        private CalendarEvent setCalendarEventObject(string name, string location, string type, string description, string date, string startTime, string endTime, string note, int otherId)
-        {
-            DateTime startDateTime = convertStringToDateTime(date + " " + startTime);
-            DateTime endDateTime = convertStringToDateTime(date + " " + endTime);
-            calendarEvent.CalendarId = currentCalendarId;
-            calendarEvent.Type = type;
-            calendarEvent.Name = name;
-            calendarEvent.Location = location;
-            calendarEvent.Description = description;
-            calendarEvent.Date = Convert.ToDateTime(date);
-            calendarEvent.StartTime = startDateTime;
-            calendarEvent.EndTime = endDateTime;
-            calendarEvent.Note = note;
-            calendarEvent.OtherId = otherId;
-            return calendarEvent;
-        }
-
-        private CalendarEvent setCalendarEventObjectWithId(int id, string name, string type, string location, string description, string date, string startTime, string endTime, string note, int otherId)
-        {
-            DateTime startDateTime = convertStringToDateTime(date + " " + startTime);
-            DateTime endDateTime = convertStringToDateTime(date + " " + endTime);
-            calendarEvent.CalendarEventId = id;
-            calendarEvent.CalendarId = currentCalendarId;
-            calendarEvent.Type = type;
-            calendarEvent.Name = name;
-            calendarEvent.Location = location;
-            calendarEvent.Description = description;
-            calendarEvent.Date = Convert.ToDateTime(date);
-            calendarEvent.StartTime = startDateTime;
-            calendarEvent.EndTime = endDateTime;
-            calendarEvent.Note = note;
-            calendarEvent.OtherId = otherId;
-            return calendarEvent;
-        }
-
         [HttpPost]
         public JsonResult retrieveEvent(int eventId)
         {
