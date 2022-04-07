3c8a94a8c527c7d9380884cf1c37954555aa85cf
Ivan <2000584@sit.singaporetech.edu.sg>
2022-04-07 13:28:49 +0800

    Updated sorting to Strategy Pattern

---
+++ personalised_conceirge_m2/personalised_concierge_m1/Strategy/DescendingSort.cs
@@ -0,0 +1,33 @@
+﻿using personalised_concierge_m1.Models.Entities.Itineraries;
+using personalised_concierge_m1.Models.Interfaces.Itineraries;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace personalised_concierge_m1.Data.Itineraries
+{
+    public class DescendingSort : ISortStrategy
+    { 
+
+        public string[] Sort(string[] list)
+        {
+            string temp;
+            int n, i, j, l;
+
+            l = list.Length;
+            for (i = 0; i < l; i++)
+            {
+                for (j = 0; j < l - 1; j++)
+                {
+                    if (list[j].CompareTo(list[j + 1]) < 0)
+                    {
+                        temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
+                    }
+                }
+            }
+            return list;
+        }
+
+    }
+}
---
+++ personalised_conceirge_m2/personalised_concierge_m1/Strategy/Interface/ISortStrategy.cs
@@ -0,0 +1,12 @@
+﻿using personalised_concierge_m1.Models.Entities.Itineraries;
+
+
+namespace personalised_concierge_m1.Models.Interfaces.Itineraries
+{
+    public interface ISortStrategy
+    {
+        string[] Sort(string[] list);
+         
+
+    }
+}
